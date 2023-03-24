7c465c1b43e7e24337a7da0aa800887085aa3161
Prayag Sangode <prayag.rhce@gmail.com>
2023-03-24 15:34:45 +0000

    add code

---
+++ only-vm-no-os/variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_vcenter" {}
+variable "vsphere_server" {}
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_datastore" {}
+variable "vsphere_cluster" {}
+variable "vsphere_network" {}
+variable "vm_name" {}
+variable "vm_cpu_nos" {}
+variable "vm_mem_size" {}
+variable "vm_guest_id" {}
+variable "vm_disk_label" {}
+variable "vm_disk_size" {}
+
---
+++ only-vm-no-os/vm.tf
@@ -0,0 +1,64 @@
+provider "vsphere" {
+  vsphere_server       = var.vsphere_vcenter
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = "T-Systems"
+}
+
+data "vsphere_datastore" "datastore" {
+  #name          = "DS4200-2"
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+#data "vsphere_datastore_cluster" "datastore_cluster" {
+#  name          = "datastore-cluster-01"
+#  datacenter_id = data.vsphere_datacenter.datacenter.id
+#}
+
+data "vsphere_compute_cluster" "cluster" {
+  #name          = "CAS"
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_network" "network" {
+  #name          = "VM Network"
+  name          = var.vsphere_network
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  #name             = "web-server11"
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  #datastore_cluster_id = data.vsphere_datastore_cluster.datastore_cluster.id
+  #num_cpus                   = 1
+  num_cpus = var.vm_cpu_nos
+  #memory                     = 1024
+  memory = var.vm_mem_size
+  #guest_id                   = "centos7_64Guest"
+  guest_id                   = var.vm_guest_id
+  enable_disk_uuid           = true
+  wait_for_guest_net_timeout = 0
+  network_interface {
+    network_id = data.vsphere_network.network.id
+    #ipv4_address       = "192.168.200.48"
+    #ipv4_prefix_length = 24
+    #ipv4_netmask = 24
+    #ipv4_gateway = "192.168.200.1"
+  }
+  disk {
+    #label            = "centos7"
+    label            = var.vm_disk_label
+    #size             = 20
+    size             = var.vm_disk_size
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+}
