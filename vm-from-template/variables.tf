336199eb514941a0a72318b8c132cc0eea669acb
root <root@management-node.example.com>
2023-04-19 05:32:32 +0000

    update code

--- vm-from-template/variables.tf
+++ vm-from-template/variables.tf
@@ -5,10 +5,12 @@ variable "vsphere_password" {}
 variable "vsphere_datastore" {}
 variable "vsphere_cluster" {}
 variable "vsphere_network" {}
-variable "vm_name" {}
+variable "vm_name1" {}
+variable "vm_name2" {}
 variable "vm_cpu_nos" {}
 variable "vm_mem_size" {}
 #variable "vm_guest_id" {}
-variable "vm_disk_label" {}
 variable "vm_template_name" {}
 variable "vm_disk_size" {}
+variable "vm_one_disk_label" {}
+variable "vm_two_disk_label" {}
--- vm-from-template/vm.tf
+++ vm-from-template/vm.tf
@@ -39,9 +39,9 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  #name             = "web-server11"
-  name             = var.vm_name
+resource "vsphere_virtual_machine" "vm1" {
+  #name             = ""
+  name             = var.vm_name1
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
   #datastore_cluster_id = data.vsphere_datastore_cluster.datastore_cluster.id
@@ -64,19 +64,69 @@ resource "vsphere_virtual_machine" "vm" {
   }
   disk {
     #label            = "centos7"
-    label = var.vm_disk_label
+    label = var.vm_one_disk_label
     #size             = 80
-    size             = var.vm_disk_size
+    size = var.vm_disk_size
     #eagerly_scrub    = false
-    #thin_provisioned = false
+    thin_provisioned = false
   }
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
 
     customize {
       linux_options {
-        host_name = "ubuntu2004"
-        domain    = "ubuntu2004.example.com"
+        host_name = "k8s-master"
+        domain    = "k8s-master.example.com"
+      }
+
+      network_interface {
+        ipv4_address    = "192.168.200.47"
+        ipv4_netmask    = 24
+        dns_server_list = ["192.168.200.1", "8.8.8.8"]
+      }
+
+      ipv4_gateway = "192.168.200.1"
+    }
+  }
+}
+resource "vsphere_virtual_machine" "vm2" {
+  #name             = ""
+  name             = var.vm_name2
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  #datastore_cluster_id = data.vsphere_datastore_cluster.datastore_cluster.id
+  #num_cpus                   = 1
+  num_cpus = var.vm_cpu_nos
+  #memory                     = 1024
+  memory    = var.vm_mem_size
+  guest_id  = data.vsphere_virtual_machine.template.guest_id
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  #guest_id                   = "centos7_64Guest"
+  #guest_id                   = var.vm_guest_id
+  #enable_disk_uuid           = true
+  wait_for_guest_net_timeout = 0
+  network_interface {
+    network_id = data.vsphere_network.network.id
+    #ipv4_address       = "192.168.200.48"
+    #ipv4_prefix_length = 24
+    #ipv4_netmask = 24
+    #ipv4_gateway = "192.168.200.1"
+  }
+  disk {
+    #label            = "centos7"
+    label = var.vm_two_disk_label
+    #size             = 80
+    size = var.vm_disk_size
+    #eagerly_scrub    = false
+    thin_provisioned = false
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "k8s-worker"
+        domain    = "k8s-worker.example.com"
       }
 
       network_interface {
@@ -88,7 +138,6 @@ resource "vsphere_virtual_machine" "vm" {
       ipv4_gateway = "192.168.200.1"
     }
   }
-
   # Execute script on remote vm after this creation
   #provisioner "remote-exec" {
   #script = "scripts/example-script.sh"
